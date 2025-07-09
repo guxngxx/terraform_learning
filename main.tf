c57b6b2f228eeaa6e5b917b47d0b1ece5c95d43b
guang <gcchong2002@gmail.com>
2025-07-09 16:17:18 +0800

    4.2 - HCL (create ec2 instance)

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
-resource "random_string" "random" {
-  length = 10
+# resource "random_string" "random" {
+#   length = 10
+# }
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-southeast-5"
+}
+
+resource "aws_instance" "web" {
+  ami = "ami-02454c79b877e3c97"
+  instance_type = "t3.micro"
+
+  subnet_id = "subnet-06dd3c17713ca845d"
+  vpc_security_group_ids = ["sg-02f2d9d1daf900579"]
+
+  tags = {
+  "Terraform" = "true"
+  }
 }
