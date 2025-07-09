6e17544e2cec71887809e78e119e630202ac749f
guang <gcchong2002@gmail.com>
2025-07-09 22:14:07 +0800

    4.10 - Introduction to the Module Block

--- main.tf
+++ main.tf
@@ -143,3 +143,24 @@ provider "aws" {
 #   }
 # }
 
+// Lab 4.10 - Introduction to the Module Block -------------------------------------------------------------------
+ module "subnet_addrs" {
+  source  = "hashicorp/subnets/cidr"
+  version = "1.0.0"
+  
+  base_cidr_block = "10.0.0.0/22"
+  networks = [
+  {
+    name     = "module_network_a"
+    new_bits = 2
+  },
+  {
+    name     = "module_network_b"
+    new_bits = 2
+  },
+ ]
+}
+
+output "subnet_addrs" {
+  value = module.subnet_addrs.network_cidr_blocks
+}
