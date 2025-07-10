611ed888a5aa0c4c695251ddb40b21b629d79445
guang <gcchong2002@gmail.com>
2025-07-10 13:44:29 +0800

    Lab 4.14 - Multiple Terraform Providers

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,6 @@
-// Lab 4.9 - Terraform Configuration Block & Lab 4.13 - Terraform Provider Installation ----------------------------------
+// Lab 4.9  - Terraform Configuration Block ---------------------------------------------------------------------
+// Lab 4.13 - Terraform Provider Installation -------------------------------------------------------------------
+// Lab 4.14 - Multiple Terraform Providers ----------------------------------------------------------------------
 terraform {
   required_version = ">= 1.0.0"
   required_providers {
@@ -6,9 +8,17 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+    http = {
+      source = "hashicorp/http"
+      version = "3.5.0"
+    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.0"
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "2.5.3"
     }
   }
 }
