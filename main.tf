07f56d122380ec125ec102399980fff144dbe9c1
guang <gcchong2002@gmail.com>
2025-07-10 13:51:14 +0800

    Lab 4.15 - Generate SSH Key with Terraform TLS Provider

--- main.tf
+++ main.tf
@@ -165,3 +165,12 @@ provider "aws" {
 #   value = module.subnet_addrs.network_cidr_blocks
 # }
 
+// Lab 4.15 - Generate SSH Key with Terraform TLS Provider --------------------------------------------------------
+resource "tls_private_key" "generated" {
+  algorithm = "RSA"
+}
+
+resource "local_file" "private_key_pem" {
+  content  = tls_private_key.generated.private_key_pem
+  filename = "MyAWSKey.pem"
+}
--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,7 @@
 // Lab 4.9  - Terraform Configuration Block ---------------------------------------------------------------------
 // Lab 4.13 - Terraform Provider Installation -------------------------------------------------------------------
 // Lab 4.14 - Multiple Terraform Providers ----------------------------------------------------------------------
+// Lab 4.15 - Generate SSH Key with Terraform TLS Provider ------------------------------------------------------
 terraform {
   required_version = ">= 1.0.0"
   required_providers {
@@ -9,7 +10,7 @@ terraform {
       version = "~> 6.0"
     }
     http = {
-      source = "hashicorp/http"
+      source  = "hashicorp/http"
       version = "3.5.0"
     }
     random = {
@@ -17,8 +18,12 @@ terraform {
       version = "~> 3.0"
     }
     local = {
-      source = "hashicorp/local"
+      source  = "hashicorp/local"
       version = "2.5.3"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.1.0"
     }
   }
 }
