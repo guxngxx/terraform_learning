08d763bfc1259b8e271ce195605f4a0b3514d27b
guang <gcchong2002@gmail.com>
2025-07-13 00:31:36 +0800

    Lab 9.8 - Terraform Built-in Functions

--- main.tf
+++ main.tf
@@ -439,3 +439,62 @@ output "data-bucket-domain-name" {
 output "data-bucket-region" {
   value = "The ${data.aws_s3_bucket.data_bucket.id} bucket is located in ${data.aws_s3_bucket.data_bucket.region}"
 }
+
+// Lab 9.8 - Terraform Built-in Functions --------------------------------------------------------------------------------
+locals {
+  maximum = max(var.num_1, var.num_2, var.num_3)
+  minimum = min(var.num_1, var.num_2, var.num_3, 44, 20)
+}
+
+output "max_value" {
+  value = local.maximum
+}
+
+output "min_value" {
+  value = local.minimum
+}
+
+# Define the VPC
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = upper("testing123")
+    // Name        = upper(var.vpc_name)
+    Environment = upper("dev")
+    // Environment = upper(var.environment)
+    Terraform = upper("true")
+  }
+
+  enable_dns_hostnames = true
+}
+
+locals {
+  # Common tags to be assigned to all resources
+  common_tags = {
+    Name = join("-",
+      [
+        "testing123",
+        "testing123",
+        "testing123"
+      ]
+    )
+    Owner     = lower("testing123")
+    App       = lower("testing123")
+    Service   = lower("testing123")
+    AppTeam   = lower("testing123")
+    CreatedBy = lower("testing123")
+  }
+}
+
+# resource "aws_subnet" "private_subnets" {
+#   for_each          = var.private_subnets
+#   vpc_id            = aws_vpc.vpc.id
+#   cidr_block        = cidrsubnet("10.0.0.0/16", 8, each.value + 100)
+#   availability_zone = tolist("data.aws_availability_zones.available.names")[each.value]
+
+#   tags = {
+#     Name      = each.key
+#     Terraform = "true"
+#   }
+# }
--- variables.tf
+++ variables.tf
@@ -57,3 +57,22 @@ variable "env" {
     }
   }
 }
+
+// Lab 9.8 - Terraform Built-in Functions --------------------------------------------------------------------------------
+variable "num_1" {
+  type        = number
+  description = "Numbers for function labs"
+  default     = 88
+}
+
+variable "num_2" {
+  type        = number
+  description = "Numbers for function labs"
+  default     = 73
+}
+
+variable "num_3" {
+  type        = number
+  description = "Numbers for function labs"
+  default     = 52
+}
