cf29e22a1189303e5e60c0c632d08e5c7b57c1a7
guang <gcchong2002@gmail.com>
2025-07-13 00:07:38 +0800

    Lab 9.7 - Working with Data Blocks

--- main.tf
+++ main.tf
@@ -404,3 +404,38 @@ resource "aws_subnet" "list_subnet3" {
   cidr_block        = each.value.ip
   availability_zone = each.value.az
 }
+
+// Lab 9.7 - Working with Data Blocks -----------------------------------------------------------------------------------
+data "aws_s3_bucket" "data_bucket" {
+  bucket = "my-data-lookup-bucket-btk"
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "data_bucket_policy"
+  description = "Allow access to my bucket"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:Get*",
+          "s3:List*"
+        ],
+        "Resource" : "${data.aws_s3_bucket.data_bucket.arn}"
+      }
+    ]
+  })
+}
+
+output "data-bucket-arn" {
+  value = data.aws_s3_bucket.data_bucket.arn
+}
+
+output "data-bucket-domain-name" {
+  value = data.aws_s3_bucket.data_bucket.bucket_domain_name
+}
+
+output "data-bucket-region" {
+  value = "The ${data.aws_s3_bucket.data_bucket.id} bucket is located in ${data.aws_s3_bucket.data_bucket.region}"
+}
